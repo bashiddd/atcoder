f5fa2ecc0c5251be04c34d611042fcee7bc1c158
bashi <bashi57.d@gmail.com>
2022-02-15 23:27:14 +0900

    edited

--- ARC-134/A.rb
+++ ARC-134/A.rb
@@ -1,15 +1,16 @@
 N, L, W = gets.chomp.split(' ').map(&:to_i)
 as = gets.chomp.split(' ').map(&:to_i)
-as.push(L)
 
 count = 0
 prev = 0
 
+
 as.each do |a|
     if a > prev
-        count += (a - prev + (L - a + W) )/ W
-    else
-        count += (L - (prev + W) ) / W
+        count += ( (a - prev).to_f/W ).ceil
+    end
+    if a == as.last
+        count += ( (L - (a + W)).to_f / W ).ceil
     end
     prev = a + W
 end
