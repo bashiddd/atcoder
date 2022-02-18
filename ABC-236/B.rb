434ace9656f8e825ed3556400c2ded883df84dfb
bashi <bashi57.d@gmail.com>
2022-02-18 16:40:17 +0900

    I did it!!!!!!!!

---
+++ ABC-236/B.rb
@@ -0,0 +1,13 @@
+N = gets.chomp.to_i
+As = gets.chomp.split(' ').map(&:to_i)
+
+ary = Array.new
+
+N.times do |num|
+    ary.push(num + 1)
+    ary.push(num + 1)
+    ary.push(num + 1)
+    ary.push(num + 1)
+end
+
+p ary.sum - As.sum
