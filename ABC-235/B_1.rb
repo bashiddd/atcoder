6b6a37e96e1eb620f65d747452e8cdf6f3105914
bashi <bashi57.d@gmail.com>
2022-02-23 23:41:39 +0900

    edited

--- ABC-235/B_1.rb
+++ ABC-235/B_1.rb
@@ -1,5 +1,12 @@
 N = gets.chomp.to_i
-Hs = gets.chomp.split.map(&:to_i)
+hs = gets.chomp.split.map(&:to_i)
 
 ary = Array.new
-Hs.each {|H| ary.push(H) if }
+hs.each_with_index {|h,i|
+    ary.push(h) if h[i] < h[i+1]
+    # h[i] < h[i+1] ? ary.push(h) : break
+    # ary.push(h) 
+    # break if h[i] >= h[i+1]
+}
+
+p ary
