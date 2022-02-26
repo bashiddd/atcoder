3e4744b2089fb98ade0b011dc12e7801ae506f6b
bashi <bashi57.d@gmail.com>
2022-02-26 23:06:20 +0900

    array.find_index

---
+++ ABC-241/B_1.rb
@@ -0,0 +1,16 @@
+N,M = gets.split.map(&:to_i)
+as = gets.split.map(&:to_i)
+bs = gets.split.map(&:to_i)
+
+bs.each_with_index {|b,i| 
+    if as.include?(b) == false
+        puts "No"
+        break
+    end
+
+    if as.include?(b) == true && i == (bs.size - 1)
+        puts "Yes"
+    elsif as.include?(b) == true
+        as.delete_at(as.find_index(b))
+    end
+}
