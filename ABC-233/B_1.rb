a1d3c2ec07ddbd3c9db2f4550987cc0d80d07628
bashi <bashi57.d@gmail.com>
2022-02-28 00:57:41 +0900

    create

--- ABC-233/B_1.rb
+++ ABC-233/B_1.rb
@@ -1,3 +1,11 @@
-L,R = gets.split.map(&:to_i)
+l,r = gets.split.map(&:to_i)
 S = gets.chomp
-puts S
+if l == 1 && r != S.size
+    puts S[0..r-1] + S[r..-1]
+end
+if l != 1 && r != S.size
+    puts S[0..l-2]+S[l-1..r-1].reverse+S[r..-1]
+end
+if l != 1 && r == S.size
+    puts S[0..l-2] + S[l-1..-1].reverse
+end
