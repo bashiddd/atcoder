e0fdabadab20ce0e64460f9235934c936c8b7b3a
bashi <bashi57.d@gmail.com>
2022-03-09 00:29:23 +0900

    edited

--- ABC-232/B_1.rb
+++ ABC-232/B_1.rb
@@ -1,8 +1,25 @@
-S = gets.split("")
-T = gets.split("")
+S = gets.chomp.split("")
+T = gets.chomp.split("")
+
+ST = []
+(S.size).times {|i|
+    ST.push([S[i],T[i]])
+}
 
 ary = ("a".."z").to_a
 
-S.each {|char|
-    char.
+dif = []
+
+(ST.size).times {|i|
+    if ary.index("#{ST[i][1]}") >= ary.index("#{ST[i][0]}")
+        dif.push(ary.index("#{ST[i][1]}") - ary.index("#{ST[i][0]}"))
+    else
+        dif.push(ary.index("#{ST[i][1]}") - ary.index("#{ST[i][0]}") + 26)
+    end
 }
+
+if dif.uniq.size == 1
+    puts "Yes"
+else
+    puts "No"
+end
