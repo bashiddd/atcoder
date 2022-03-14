a7bb9b62ded06b6cddbf53e835bc6dc477eddff0
bashi <bashi57.d@gmail.com>
2022-03-14 23:57:35 +0900

    edited

--- ABC-243/C_1.rb
+++ ABC-243/C_1.rb
@@ -7,13 +7,13 @@ ary1 = []
 ary.each_with_index {|a,i|
     ary1.push([a[1],s[i]])
 }
-p ary1
+
 ary2 = []
 
 ary1.uniq.each {|a|
     ary2.push(a[1])
 }
-p ary2
+
 if ary2.size == 2
     puts "Yes"
 else
