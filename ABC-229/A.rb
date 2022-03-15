989cfd3812aeb6fa7d7ea4ed80d348f697a53254
bashi <bashi57.d@gmail.com>
2022-03-16 00:16:31 +0900

    edited

--- ABC-229/A.rb
+++ ABC-229/A.rb
@@ -1,2 +1,8 @@
-s1 = gets.chomp
-s2 = gets.chomp
+s1 = gets.chomp.split("")
+s2 = gets.chomp.split("")
+
+if (s1==["#","."] && s2==[".","#"]) || (s1==[".","#"] && s2==["#","."])
+    puts "No"
+else
+    puts "Yes"
+end
