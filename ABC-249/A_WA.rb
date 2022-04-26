b7307ba17b11086b6b2e2a0d1c39ad71f2d654ad
bashi <bashi57.d@gmail.com>
2022-04-27 00:37:44 +0900

    Aにしてはむず

---
+++ ABC-249/A_WA.rb
@@ -0,0 +1,26 @@
+a,b,c,d,e,f,x = gets.split.map(&:to_i)
+ta,ao = 0,0
+
+if a>x
+    ta=x*b
+elsif x/(a+c)==0
+    ta=a*b
+elsif x/(a+c)>0
+    ta = ((x/(a+c)) * a * b) + ((x - (x/(a+c))*a) * b)
+end
+
+if d>x
+    ao=x*e
+elsif x/(d+f)==0
+    ao=c*e
+elsif x/(d+f)>0
+    ao = ((x/(d+f)) * d * e) + ((x - (x/(d+f))*d) * e)
+end
+
+if ta>ao
+    puts "Takahashi"
+elsif ao>ta
+    puts "Aoki"
+elsif ta==ao
+    puts "Draw"
+end
