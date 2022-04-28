4615f88357939a6578ff0fddd782dd3daad7e5e3
bashi <bashi57.d@gmail.com>
2022-04-29 00:17:35 +0900

    [].sizeは0 nilじゃない

--- ABC-249/B_1.rb
+++ ABC-249/B_1.rb
@@ -1,2 +1,9 @@
-p ("A".."Z").to_a
-p ("a".."z").to_a
+s = gets.chomp.chars
+upper = ("A".."Z").to_a
+lower = ("a".."z").to_a
+
+if (s&upper).size!=0 && (s&lower).size!=0 && (s.size==s.uniq.size)
+    puts "Yes"
+else
+    puts "No"
+end
