ef59dbc13303ff72fe6ecf5f2da8eb7de88c8d80
bashi <bashi57.d@gmail.com>
2022-04-28 23:52:47 +0900

    create

--- ABC-249/A_1.rb
+++ ABC-249/A_1.rb
@@ -1,19 +1,19 @@
 a,b,c,d,e,f,x = gets.split.map(&:to_i)
 ta,ao = 0,0
 
-if a>x
+if a>=x
     ta=x*b
-elsif x/(a+c)==0
+elsif x/(a+c)==0 && a<x
     ta=a*b
-elsif x/(a+c)>0
+elsif x/(a+c)>0 && a<x
     ta = ((x/(a+c)) * a * b) + ((x - (x/(a+c))*a) * b)
 end
 
-if d>x
+if d>=x
     ao=x*e
-elsif x/(d+f)==0
+elsif x/(d+f)==0 && b<x
     ao=c*e
-elsif x/(d+f)>0
+elsif x/(d+f)>0 && b<x
     ao = ((x/(d+f)) * d * e) + ((x - (x/(d+f))*d) * e)
 end
 
