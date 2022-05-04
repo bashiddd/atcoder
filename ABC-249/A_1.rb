1b40eab3cc86de226a907e5743fcb314ef52a01d
bashi <bashi57.d@gmail.com>
2022-05-04 23:30:30 +0900

    無理やりすぎるな

--- ABC-249/A_1.rb
+++ ABC-249/A_1.rb
@@ -1,20 +1,28 @@
 a,b,c,d,e,f,x = gets.split.map(&:to_i)
 ta,ao = 0,0
 
-if a>=x
+if a>x
     ta=x*b
-elsif x/(a+c)==0 && a<x
+elsif x/(a+c)==0
     ta=a*b
-elsif x/(a+c)>0 && a<x
-    ta = ((x/(a+c)) * a * b) + ((x - (x/(a+c))*a) * b)
+elsif x/(a+c)>0
+    if (x - ((x/(a+c))*(a+c))) < a
+        ta = ((x/(a+c)) * a * b) + ((x - ((x/(a+c))*(a+c))) * b)
+    else
+        ta = ((x/(a+c)) * a * b) + (a * b)
+    end
 end
 
-if d>=x
+if d>x
     ao=x*e
-elsif x/(d+f)==0 && b<x
+elsif x/(d+f)==0
     ao=c*e
-elsif x/(d+f)>0 && b<x
-    ao = ((x/(d+f)) * d * e) + ((x - (x/(d+f))*d) * e)
+elsif x/(d+f)>0
+    if (x - ((x/(d+f))*(d+f))) < d
+        ao = ((x/(d+f)) * d * e) + ((x - ((x/(d+f))*(d+f))) * e)
+    else
+        ao = ((x/(d+f)) * d * e) + (d * e)
+    end
 end
 
 if ta>ao
