e0b9d4333f10572af1772a4ecf3526caaa680654
bashi <bashi57.d@gmail.com>
2022-05-24 23:15:45 +0900

    めっちゃループ

--- ABC-220/B.rb
+++ ABC-220/B.rb
@@ -1,3 +1,9 @@
-k = gets.to_i
-a, b = gets.chomp.split.map(&:to_s)
-puts a.to_i(k) * b.to_i(k)
+# k = gets.to_i
+# a, b = gets.chomp.split.map(&:to_s)
+# puts a.to_i(k) * b.to_i(k)
+
+# a = [[1,1],[2,2],[3,3]]
+# b = a.delete_at(1)
+# p b
+a = [1,1,2,3,4,5]
+p a.select {_1==a.min}
--- ABc-214/B.rb
+++ ABc-214/B.rb
@@ -1,1 +1,10 @@
 s,t = gets.split.map(&:to_i)
+answer = 0
+(0..s).each {|a|
+    (0..s).each {|b|
+        (0..s).each {|c|
+            answer+=1 if a+b+c<=s && a*b*c<=t
+        }
+    }
+}
+puts answer
