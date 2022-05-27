fc2122960f1eaae543a07d24880a332810ea353d
bashi <bashi57.d@gmail.com>
2022-05-28 00:36:16 +0900

    Aにしてむずい

--- ABC-213/A.rb
+++ ABC-213/A.rb
@@ -1,1 +1,13 @@
 a,b = gets.split.map(&:to_i)
+a = a.to_s(2).reverse.split("")
+b = b.to_s(2).reverse.split("")
+c = []
+b.each_with_index {|num,i|
+    if num=="1" && a[i]=="1"
+        c.push(0)
+    else
+        c.push(1)
+    end
+    p c
+}
+# puts c.join
