93004f1f87bca7436f27ed3ffa247c47eb1fd901
bashi <bashi57.d@gmail.com>
2022-06-03 23:59:44 +0900

    三項演算子 代入

---
+++ ABC-210/B.rb
@@ -0,0 +1,10 @@
+n = gets.to_i
+s = gets.split("").map(&:to_i)
+answer = "Takahashi"
+s.each {
+    if _1==1
+        puts answer
+        exit
+    end
+    answer = answer=="Takahashi" ? "Aoki" : "Takahashi"
+}
