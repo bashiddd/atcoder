f8971378aa080071a58e6760ccd7ca5ecc537d9e
bashi <bashi57.d@gmail.com>
2022-06-04 22:53:55 +0900

    多分もっといい方法あるな

---
+++ ABC-254/B.rb
@@ -0,0 +1,14 @@
+n = gets.to_i
+answer = []
+n.times {|i|
+    ary = Array.new(i+1)
+    ary[0],ary[-1] = 1,1
+    if i>=2
+        (i-1).times {|x|
+            ary[x+1]=answer[i-1][x]+answer[i-1][x+1]
+        }
+    end
+    # p ary
+    answer.push(ary)
+    puts ary.join(" ")
+}
