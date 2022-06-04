d3c3d1d34fd7a240884498902c283d566445a5b5
bashi <bashi57.d@gmail.com>
2022-06-04 23:09:19 +0900

    l7 timesの前で計算が必要な場合は( )つける!こいつのせいでレート落ちた!

---
+++ ABC-254/B_WA.rb
@@ -0,0 +1,13 @@
+n = gets.to_i
+answer = []
+n.times {|i|
+    ary = Array.new(i+1)
+    ary[0],ary[-1] = 1,1
+    if i>=2
+        i-1.times {|x|
+            ary[x+1]=answer[i-1][x]+answer[i-1][x+1]
+        }
+    end
+    answer.push(ary)
+    puts ary.join(" ")
+}
