be1f077ae10b8664672229541378983277335a42
bashi <bashi57.d@gmail.com>
2022-07-17 00:10:29 +0900

    多分もっと良い方法あるな

--- ABC-182/B.rb
+++ ABC-182/B.rb
@@ -1,6 +1,10 @@
 n = gets.to_i
 a = gets.split.map(&:to_i)
 
-(2..a.max).each {
-    
+gcd = []
+(2..a.max).each {|number|
+    gcd.push(a.count {_1%number==0})
 }
+
+max = gcd.max
+puts gcd.index(max)+2
