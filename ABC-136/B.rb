d2cce74c5458c3c0ff60d3d70a632430f4f27a42
bashi <bashi57.d@gmail.com>
2022-10-20 23:22:15 +0900

    綺麗じゃないな

--- ABC-136/B.rb
+++ ABC-136/B.rb
@@ -1,13 +1,17 @@
-n = gets.to_i
+# n = gets.to_i
 
-nsize = n.size
-answer = 0
-(nsize.to_f/2).ceil.times {|i|
-    if i==nsize
-        answer += (("1"+"0"*(2i+1)).to_i..n).to_a.size
-        exit
-    end
-    answer += (("1"+"0"*(2i+1)).to_i..("9"*(2i+1)).to_i).to_a.size
-}
+# nsize = n.to_s.size
+# p nsize
+# answer = 0
+# p (nsize.to_f/2).ceil
+# (nsize.to_f/2).ceil.times {|i|
+#     if i==nsize
+#         answer += (("1"+"0"*(2*i+1)).to_i..n).to_a.size
+#         break
+#     end
+#     answer += (("1"+"0"*(2*i+1)).to_i..("9"*(2*i+1)).to_i).to_a.size
+# }
 
-p answer
+# p answer
+i = 0
+p (("1"+"0"*(2*i+1)).to_i..("9"*(2*i+1)).to_i).to_a
